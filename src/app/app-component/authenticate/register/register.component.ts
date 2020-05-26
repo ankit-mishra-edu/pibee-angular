82e3130d6a5590eda544d6a410b3f1adfce36efb
Ankit Mishra <amishm766@gmail.com>
2020-05-27 01:05:23 +0530

    Added the support to show the danger text for error in register.
    Added an icon and now the error will be shown in the same line.Also added the validation for pristine and valid.

--- src/app/app-component/authenticate/register/register.component.ts
+++ src/app/app-component/authenticate/register/register.component.ts
@@ -7,66 +7,63 @@ import { IUser } from 'src/app/app-interface/User';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit, OnDestroy {
-
   userData;
-  usersArray : IUser[];
+  usersArray: IUser[];
   subscriptions = new SubSink();
-  constructor(private _auth : AuthService, private _route : Router) { }
-  
+  constructor(private _auth: AuthService, private _route: Router) {}
+
   ngOnInit(): void {
     this.userData = {
-      "user" : {
-        "username" : '' ,
-        "first_name" : '' ,
-        "last_name" : '' ,
-        "email" : '',
-        "password" : ''
-      }
-    }
+      user: {
+        username: '',
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+      },
+    };
     this.getAllUsers();
   }
 
   getAllUsers() {
-    this.subscriptions.sink = this._auth.GetAllUsers()
-    .subscribe(
-      getAllUsersResponse => {
+    this.subscriptions.sink = this._auth.GetAllUsers().subscribe(
+      (getAllUsersResponse) => {
         console.log(getAllUsersResponse);
         this.usersArray = getAllUsersResponse;
       },
 
-      getAllUsersError => {
+      (getAllUsersError) => {
         console.log(getAllUsersError);
       },
 
-      () => {console.log("All Users fetched successfully");}
+      () => {
+        console.log('All Users fetched successfully');
+      }
     );
-    
   }
 
-  signUp(){
-    this.subscriptions.sink = this._auth.Register(this.userData)
-    .subscribe(
-      signUpResponse => {
-        localStorage.setItem('Token', signUpResponse.key)
-        localStorage.setItem('user', signUpResponse.user.toString())
-        console.log(signUpResponse)
+  signUp() {
+    this.subscriptions.sink = this._auth.Register(this.userData).subscribe(
+      (signUpResponse) => {
+        localStorage.setItem('Token', signUpResponse.key);
+        localStorage.setItem('user', signUpResponse.user.toString());
+        console.log(signUpResponse);
       },
-      signUpError => {
-        console.log(signUpError)
-        alert(signUpError.error)
+      (signUpError) => {
+        console.log(signUpError);
+        alert(signUpError.error);
       },
       () => {
-        console.log("Sign up Successful")
-        this._route.navigate(['login'])
+        console.log('Sign up Successful');
+        this._route.navigate(['login']);
       }
-    )
+    );
   }
 
   ngOnDestroy() {
     this.subscriptions.unsubscribe();
   }
-
 }
