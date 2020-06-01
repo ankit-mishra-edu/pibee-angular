84677edf461c12bf58c03f572b60c13037fbfd55
Ankit Mishra <amishm766@gmail.com>
2020-06-01 22:42:50 +0530

    Added MainComponent and FooterComponent and moved all other components to main.
    Shifted footer part to footer component and other than navbar and footer rest all to main.So now only there are thre outer component Navbar, Main and Footer component.Main contains all other components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './app-component/home/home.component';
-import { LoginComponent } from './app-component/authenticate/login/login.component';
-import { ChatappComponent } from './app-component/chatapp/chatapp.component';
-import { RegisterComponent } from './app-component/authenticate/register/register.component';
-import { LogoutComponent } from './app-component/authenticate/logout/logout.component';
-import { EditProfileComponent } from './app-component/authenticate/profile/edit-profile/edit-profile.component';
-import { ViewProfileComponent } from './app-component/authenticate/profile/view-profile/view-profile.component';
-import { EditDetailsComponent } from './app-component/authenticate/edit-details/edit-details.component';
-import { ChangePasswordComponent } from './app-component/authenticate/change-password/change-password.component';
+import { HomeComponent } from './app-component/main/home/home.component';
+import { LoginComponent } from './app-component/main/authenticate/login/login.component';
+import { ChatappComponent } from './app-component/main/chatapp/chatapp.component';
+import { RegisterComponent } from './app-component/main/authenticate/register/register.component';
+import { LogoutComponent } from './app-component/main/authenticate/logout/logout.component';
+import { EditProfileComponent } from './app-component/main/authenticate/profile/edit-profile/edit-profile.component';
+import { ViewProfileComponent } from './app-component/main/authenticate/profile/view-profile/view-profile.component';
+import { EditDetailsComponent } from './app-component/main/authenticate/edit-details/edit-details.component';
+import { ChangePasswordComponent } from './app-component/main/authenticate/change-password/change-password.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './app-component/home/home.component';
-import { LoginComponent } from './app-component/authenticate/login/login.component';
-import { RegisterComponent } from './app-component/authenticate/register/register.component';
-import { ChatappComponent } from './app-component/chatapp/chatapp.component';
-import { LogoutComponent } from './app-component/authenticate/logout/logout.component';
-import { EditProfileComponent } from './app-component/authenticate/profile/edit-profile/edit-profile.component';
-import { ViewProfileComponent } from './app-component/authenticate/profile/view-profile/view-profile.component';
-import { EditDetailsComponent } from './app-component/authenticate/edit-details/edit-details.component';
-import { ChangePasswordComponent } from './app-component/authenticate/change-password/change-password.component';
+import { HomeComponent } from './app-component/main/home/home.component';
+import { LoginComponent } from './app-component/main/authenticate/login/login.component';
+import { RegisterComponent } from './app-component/main/authenticate/register/register.component';
+import { ChatappComponent } from './app-component/main/chatapp/chatapp.component';
+import { LogoutComponent } from './app-component/main/authenticate/logout/logout.component';
+import { EditProfileComponent } from './app-component/main/authenticate/profile/edit-profile/edit-profile.component';
+import { ViewProfileComponent } from './app-component/main/authenticate/profile/view-profile/view-profile.component';
+import { EditDetailsComponent } from './app-component/main/authenticate/edit-details/edit-details.component';
+import { ChangePasswordComponent } from './app-component/main/authenticate/change-password/change-password.component';
 import { NavbarComponent } from './app-component/navbar/navbar.component';
+import { MainComponent } from './app-component/main/main.component';
+import { FooterComponent } from './app-component/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +31,8 @@ import { NavbarComponent } from './app-component/navbar/navbar.component';
     ViewProfileComponent,
     EditDetailsComponent,
     ChangePasswordComponent,
+    MainComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
