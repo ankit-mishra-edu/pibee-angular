7efe1ab7c9032e627e6876669cfb2d61dd228d1f
amishm766 <amishm766@gmail.com>
2020-08-22 18:33:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 //Install express server
 const express = require("express");
 const path = require("path");
-const compression = require("compression");
+// const compression = require("compression");
 const app = express();
-app.use(compression());
+// app.use(compression());
 
 // Serve only the static files form the dist directory
 app.use(express.static("./dist/piBeeAngular"));
@@ -13,4 +13,4 @@ app.get("/*", function (req, res) {
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200, "127.0.0.1");
+app.listen(process.env.PORT || 8080);
