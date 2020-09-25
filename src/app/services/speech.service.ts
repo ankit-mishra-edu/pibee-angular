1f60e84112039fe305cc60be06b97e04714c381b
amishm766 <amishm766@gmail.com>
2020-09-26 01:50:01 +0530

    UI Change

--- src/app/services/speech.service.ts
+++ src/app/services/speech.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { IWindow } from '../modules/shared/interfaces/Speech';
 
 @Injectable({
   providedIn: 'root',
